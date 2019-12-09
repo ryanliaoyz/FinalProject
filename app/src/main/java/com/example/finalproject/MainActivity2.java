af025d259b71c6b78bd755acd6d4c9da860cb55b
Junkai-Wu <junkai2@illinois.edu>
2019-12-09 03:43:57 -0600

    hi

--- app/src/main/java/com/example/finalproject/MainActivity2.java
+++ app/src/main/java/com/example/finalproject/MainActivity2.java
@@ -9,26 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity2 extends AppCompatActivity {
 
     private EditText sendText;
     private Button send;
     private Button setting;
-//    private MessageAdapter messageAdapter;
-//    private ListView messageView;
+    private TextView mytext1, mytext2, mytext3, mytext4, mytext5, mytext6, mytext7, mytext8;
+    private TextView gtext1, gtext2, gtext3, gtext4, gtext5, gtext6, gtext7, gtext8;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         setupUI();
-//        messageAdapter = new MessageAdapter(this);
-        sendText = (EditText) findViewById(R.id.sendText);
-//        messageView = (ListView) findViewById(R.id.messageView);
-//        messageView.setAdapter(messageAdapter);
-        setting = findViewById(R.id.setting);
-        send = findViewById(R.id.send);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,13 +46,45 @@ public class MainActivity2 extends AppCompatActivity {
         sendText = findViewById(R.id.sendText);
         send = findViewById(R.id.send);
         setting = findViewById(R.id.setting);
+        mytext1 = findViewById(R.id.mytext1);
+        mytext2 = findViewById(R.id.mytext2);
+        mytext3 = findViewById(R.id.mytext3);
+        mytext4 = findViewById(R.id.mytext4);
+        mytext5 = findViewById(R.id.mytext5);
+        mytext6 = findViewById(R.id.mytext6);
+        mytext7 = findViewById(R.id.mytext7);
+        mytext8 = findViewById(R.id.mytext8);
+        gtext1 = findViewById(R.id.gtext1);
+        gtext2 = findViewById(R.id.gtext2);
+        gtext3 = findViewById(R.id.gtext3);
+        gtext4 = findViewById(R.id.gtext4);
+        gtext5 = findViewById(R.id.gtext5);
+        gtext6 = findViewById(R.id.gtext6);
+        gtext7 = findViewById(R.id.gtext7);
+        gtext8 = findViewById(R.id.gtext8);
+
+
+
     }
 
     private void sendMessage(View view) {
-        String messsage = sendText.getText().toString();
-        if (messsage.length() > 0) {
+        TextView[] mytext = {mytext1, mytext2, mytext3, mytext4, mytext5, mytext6, mytext7, mytext8};
+        TextView[] gtext = {gtext1, gtext2, gtext3, gtext4, gtext5, gtext6, gtext7, gtext8};
+        String Message = sendText.getText().toString();
+        if (Message.length() > 0) {
             //send message
+            for (int i = 7; i > 0; i--) {
+                String temp = mytext[i-1].getText().toString();
+                mytext[i].setText(temp);
+            }
+            mytext[0].setText(Message);
+            for (int i = 7; i > 0; i--) {
+                String temp = gtext[i-1].getText().toString();
+                gtext[i].setText(temp);
+            }
+            gtext[0].setText("Hi frustrated student");
             sendText.getText().clear();
+
         }
     }
 }
